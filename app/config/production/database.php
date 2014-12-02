c742e209a1852af190bca2af5cc0f15be3eff83c
Kate deBethune <kdebethune@gmail.com>
2014-12-02 16:52:28 -0500

    set up production database config

---
+++ app/config/production/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'p4',
+            'username'  => 'root',
+            'password'  => '7kGeLVd9I9',
+        ),
+
+    ),
+
+);
+
+?>
