430cdfee2eb525d7ef0fe247dd7f155fd22f79d3
Kate deBethune <kdebethune@gmail.com>
2014-12-04 10:43:57 -0500

    fixed dupe problem on order seeder

--- app/database/seeds/OrderTableSeeder.php
+++ app/database/seeds/OrderTableSeeder.php
@@ -37,17 +37,17 @@ class OrderTableSeeder extends Seeder {
 			# POPULATE food_order PIVOT TABLE with a random # of unique food ids.
 			#$rand = $faker->numberBetween(1, 10);
 			#for ($j = 0; $j < $rand; $j++)
+			$selected = array(0);
+			for ($k = 0; $k < $food_max_id; $k++) {
+    				array_push($selected, 0);
+    		}
+
 			for ($j = 0; $j < 5; $j++)
 			{
 				
-				$selected = array(0);
 				$rand_fd_id = $faker->numberBetween($food_min_id, $food_max_id);
 				$rand_qt = $faker->numberBetween(1, 10);
 		
-				for ($k = 0; $k < $food_max_id; $k++) {
-    				array_push($selected, 0);
-    			}
-    			
     			if ($selected[$rand_fd_id] == 1) {
     				#$rand_fd_id = $faker->numberBetween($food_min_id, $food_max_id)
     				continue;
