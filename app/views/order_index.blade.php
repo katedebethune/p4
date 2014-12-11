090908f6d8cb011dcfc1a0cb1e16a6b6281aa504
Kate deBethune <kdebethune@gmail.com>
2014-12-11 15:07:39 -0500

    adding table to order_index

--- app/views/order_index.blade.php
+++ app/views/order_index.blade.php
@@ -8,19 +8,43 @@
 
 	<h1>Orders for {{ Auth::user()->firstname; }} {{ Auth::user()->lastname;}}</h1>
 	
-	{{-- $orders --}}
-	
 	@foreach($orders as $order)
+			<table class="table table-bordered">
+			<tr>
+				<td>
 			{{'Order #'.$order['id'].': Due on '.$order['due'].'<br>' }}
+				</td>
+			</tr>
 			
 			@foreach($order->food()->select('name','quantity', 'sold_by_desc', 'extended_price', 'price')->get() as $food)
-				{{ $food['quantity'].' '.
+			<tr>
+				<td>
+					{{ $food['quantity'] }}
+				</td>
+				<td>
+					{{ $food['name'] }}
+				</td>
+				<td>
+					{{ $food['price'].' '.$food['sold_by_desc'] }}
+				</td>
+				<td>
+					{{ '$'.$food['extended_price'] }}
+				</td>
+				
+				{{-- $food['quantity'].' '.
 					$food['name'].' at $'.$food['price'].' '.
-					$food['sold_by_desc'].': $'.$food['extended_price'].'<br>' }}
+					$food['sold_by_desc'].': $'.$food['extended_price'].'<br>' --}}
+			</tr>
 			@endforeach
-			{{ 'Grand Total (excluding tax): $'.$order['order_total'].'<br>' }}
+			<tr>
+				<td>
+					{{ 'Grand Total (excluding tax): $'.$order['order_total'].'<br>' }}
+				</td>
+			</tr>
+			
 			<a class="btn btn-default btn-xs" href="/orders/edit/{{$order['id']}}">Edit or Delete</a>
 			'<br><br>'
+			</table>
 	@endforeach
 
 
