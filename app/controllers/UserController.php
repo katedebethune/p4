1e3ca519117cf7e50c6815935dbaaa3812b47836
Kate deBethune <kdebethune@gmail.com>
2014-12-15 16:09:55 -0500

    removing 'Welcome to foobooks' from user controller

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -87,7 +87,7 @@ class UserController extends BaseController {
 		# Log in
 		Auth::login($user);
 
-		return Redirect::to('/')->with('flash_message', 'Welcome to Foobooks!');
+		return Redirect::to('/')->with('flash_message', 'Welcome to Judith\'s Kitchen!');
 
 	}
 
