8bab3f112d2b0c5ad487c9c040572872f6cece25
Kate deBethune <kdebethune@gmail.com>
2014-12-15 16:55:21 -0500

    fixing validation errors; adding </ul> tag to end of menu code

--- app/start/global.php
+++ app/start/global.php
@@ -129,6 +129,7 @@ HTML::macro('nav_close', function()
 {
 	return
 	'
+   		 </ul>
    		</div><!--/.nav-collapse -->
       </div>
     </div>
@@ -199,7 +200,7 @@ HTML::macro('footer', function()
 	'
 		<div id="footerwrap">
 				<div class="container">
-						<h4>&copy; Copyright '. date('Y') .', Judith\'s Kitchen&nbsp&nbsp&nbsp&nbsp
+						<h4>&copy; Copyright '. date('Y') .', Judith\'s Kitchen&nbsp;&nbsp;&nbsp;&nbsp;
 						<a href="http://blacktie.co">Credits</a></h4>
 				</div>
 		</div>
