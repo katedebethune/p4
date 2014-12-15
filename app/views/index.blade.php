c32a91656df064a74e00a7471d2d200ec66bea3b
Kate deBethune <kdebethune@gmail.com>
2014-12-15 18:53:50 -0500

    fixing validation errors, sorting out conflicting div ids in Shield template.

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -63,25 +63,27 @@
 		
 		
 		<!-- ==== MENU ==== -->
-		<div class="container" id="menu" name="menu">
-			<br>
-			<br>
-			<div class="row">
-				<h2 class="centered">CAFE MENU</h2>
-				<hr>
+		<div class="container" data-name="menu">
+			<div id="menu">
 				<br>
-				<div class="col-lg-offset-2 col-lg-8">	
-						@foreach($cafe_menu as $food)
-  							<strong> {{ $food->name }} </strong>
-  							<br>
-  							{{ $food->description }}
-  							<br>
-  							<strong>{{ '$'.$food->price }}</strong>{{' '.$food->sold_by_desc }}
-  							<br><br>
-						@endforeach
+				<br>
+				<div class="row">
+					<h2 class="centered">CAFE MENU</h2>
+					<hr>
+					<br>
+					<div class="col-lg-offset-2 col-lg-8">	
+							@foreach($cafe_menu as $food)
+								<strong> {{ $food->name }} </strong>
+								<br>
+								{{ $food->description }}
+								<br>
+								<strong>{{ '$'.$food->price }}</strong>{{' '.$food->sold_by_desc }}
+								<br><br>
+							@endforeach
 
-				</div><!-- col-lg -->
-			</div><!-- row -->
+					</div><!-- col-lg -->
+				</div><!-- row -->
+			</div><!-- id -->
 		</div><!-- container -->
   		
 
@@ -95,33 +97,35 @@
 		</section><!-- section -->
 
 		<!-- ==== CATERING SERVICES ==== -->
-		<div class="container" id="catering" name="catering">
-		<br>
-			<!--div class="row white centered"-->
-			<div class="row">
-				<h1 class="centered">CATERING MENU</h1>
-				<hr>
-				<br>
-				<div class="col-lg-offset-2 col-lg-8">
-					@foreach($catering_menu as $food)
-  							<strong> {{ $food->name }} </strong>
-  							<br>
-  							{{ $food->description }}
-  							<br>
-  							<strong>{{ '$'.$food->price }}</strong>{{' '.$food->sold_by_desc }}
-  							<br><br>
-						@endforeach
-				</div><!-- col-lg -->
-			</div><!-- row -->
-			@if(!Auth::check())
-				<div class="col-md-4">
-					<a class="btn btn-primary btn-lg" href="orders/create">Login or Sign up to place a catering order</a>
-				</div>
-			@else
-				<div class="col-md-4">
-					<a class="btn btn-primary btn-lg" href="orders/create">Place a catering order</a>
-				</div>
-			@endif
+		<div class="container"  data-name="catering">
+			<div id="catering">
+			<br>
+				<!--div class="row white centered"-->
+				<div class="row">
+					<h1 class="centered">CATERING MENU</h1>
+					<hr>
+					<br>
+					<div class="col-lg-offset-2 col-lg-8">
+						@foreach($catering_menu as $food)
+								<strong> {{ $food->name }} </strong>
+								<br>
+								{{ $food->description }}
+								<br>
+								<strong>{{ '$'.$food->price }}</strong>{{' '.$food->sold_by_desc }}
+								<br><br>
+							@endforeach
+					</div><!-- col-lg -->
+				</div><!-- row -->
+				@if(!Auth::check())
+					<div class="col-md-4">
+						<a class="btn btn-primary btn-lg" href="orders/create">Login or Sign up to place a catering order</a>
+					</div>
+				@else
+					<div class="col-md-4">
+						<a class="btn btn-primary btn-lg" href="orders/create">Place a catering order</a>
+					</div>
+				@endif
+			</div><!-- id -->
 		</div><!-- container -->
 		<!-- END CATERING MENU -->
 
@@ -136,52 +140,53 @@
 			</div><!-- container -->
 		</section><!-- section -->
 		
-		<div class="container" id="contact" name="contact">
-			<div class="row">
-			<br>
-				<h1 class="centered">THANKS FOR VISITING</h1>
-				<hr>
+		<div class="container" data-name="contact">
+			<div id="contact>
+				<div class="row">
 				<br>
-				<br>
-				<div class="col-lg-4">
-					<h3>Contact Information</h3>
-					<p><span class="icon icon-home"></span> 1371 Washington Street, West Newton, MA 02465<br/>
-						<span class="icon icon-phone"></span> 617-xxx-xxxx <br/>
-						<!-- <span class="icon icon-mobile"></span> +34 59855 9853 <br/> -->
-						<span class="icon icon-envelop"></span> <a href="#"> info@judiths-kitchen.com</a> <br/>
-						<span class="icon icon-twitter"></span> <a href="#"> @judiths-kitchen </a> <br/>
-						<span class="icon icon-facebook"></span> <a href="#"> Judith's Kitchen </a> <br/>
-					</p>
-				</div><!-- col -->
-				{{--
-				<div class="col-lg-4">
-					<h3>Newsletter</h3>
-					<p>Register to our newsletter and be updated with the latests information regarding our services, offers and much more.</p>
-					<p>
-						<form class="form-horizontal" role="form">
-						  <div class="form-group">
-						    <label for="inputEmail1" class="col-lg-4 control-label"></label>
-						    <div class="col-lg-10">
-						      <input type="email" class="form-control" id="inputEmail1" placeholder="Email">
-						    </div>
-						  </div>
-						  <div class="form-group">
-						    <label for="text1" class="col-lg-4 control-label"></label>
-						    <div class="col-lg-10">
-						      <input type="text" class="form-control" id="text1" placeholder="Your Name">
-						    </div>
-						  </div>
-						  <div class="form-group">
-						    <div class="col-lg-10">
-						      <button type="submit" class="btn btn-success">Sign in</button>
-						    </div>
-						  </div>
-					   </form><!-- form -->
-					</p>
-				</div><!-- col -->
-				--}}
-			</div><!-- row -->
-		
+					<h1 class="centered">THANKS FOR VISITING</h1>
+					<hr>
+					<br>
+					<br>
+					<div class="col-lg-4">
+						<h3>Contact Information</h3>
+						<p><span class="icon icon-home"></span> 1371 Washington Street, West Newton, MA 02465<br/>
+							<span class="icon icon-phone"></span> 617-xxx-xxxx <br/>
+							<!-- <span class="icon icon-mobile"></span> +34 59855 9853 <br/> -->
+							<span class="icon icon-envelop"></span> <a href="#"> info@judiths-kitchen.com</a> <br/>
+							<span class="icon icon-twitter"></span> <a href="#"> @judiths-kitchen </a> <br/>
+							<span class="icon icon-facebook"></span> <a href="#"> Judith's Kitchen </a> <br/>
+						</p>
+					</div><!-- col -->
+					{{--
+					<div class="col-lg-4">
+						<h3>Newsletter</h3>
+						<p>Register to our newsletter and be updated with the latests information regarding our services, offers and much more.</p>
+						<p>
+							<form class="form-horizontal" role="form">
+							  <div class="form-group">
+								<label for="inputEmail1" class="col-lg-4 control-label"></label>
+								<div class="col-lg-10">
+								  <input type="email" class="form-control" id="inputEmail1" placeholder="Email">
+								</div>
+							  </div>
+							  <div class="form-group">
+								<label for="text1" class="col-lg-4 control-label"></label>
+								<div class="col-lg-10">
+								  <input type="text" class="form-control" id="text1" placeholder="Your Name">
+								</div>
+							  </div>
+							  <div class="form-group">
+								<div class="col-lg-10">
+								  <button type="submit" class="btn btn-success">Sign in</button>
+								</div>
+							  </div>
+						   </form><!-- form -->
+						</p>
+					</div><!-- col -->
+					--}}
+				</div><!-- row -->
+			</div><!-- id -->
 		</div><!-- container -->
 
 		{{--
