403702729ced0e84d886b0f5023cc79cb59232ee
Kate deBethune <kdebethune@gmail.com>
2014-12-15 19:03:00 -0500

    fixing validation errors, sorting out conflicting div ids in Shield template.

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -158,44 +158,10 @@
 							<span class="icon icon-facebook"></span> <a href="#"> Judith's Kitchen </a> <br/>
 						</p>
 					</div><!-- col -->
-					{{--
-					<div class="col-lg-4">
-						<h3>Newsletter</h3>
-						<p>Register to our newsletter and be updated with the latests information regarding our services, offers and much more.</p>
-						<p>
-							<form class="form-horizontal" role="form">
-							  <div class="form-group">
-								<label for="inputEmail1" class="col-lg-4 control-label"></label>
-								<div class="col-lg-10">
-								  <input type="email" class="form-control" id="inputEmail1" placeholder="Email">
-								</div>
-							  </div>
-							  <div class="form-group">
-								<label for="text1" class="col-lg-4 control-label"></label>
-								<div class="col-lg-10">
-								  <input type="text" class="form-control" id="text1" placeholder="Your Name">
-								</div>
-							  </div>
-							  <div class="form-group">
-								<div class="col-lg-10">
-								  <button type="submit" class="btn btn-success">Sign in</button>
-								</div>
-							  </div>
-						   </form><!-- form -->
-						</p>
-					</div><!-- col -->
-					--}}
 				</div><!-- row -->
 			</div><!-- id -->
 		</div><!-- container -->
 
-		{{--
-		<div id="footerwrap">
-			<div class="container">
-				<h4>Created by <a href="http://blacktie.co">BlackTie.co</a> - Copyright 2014</h4>
-			</div>
-		</div>
-		--}}
 {{-- END OF SHIELD THEME --}}
 
 
