22bcda7c3910a5bf9023116de082ebc750388db3
Kate deBethune <kdebethune@gmail.com>
2015-05-20 18:24:43 -0400

    implemented foco font for testing

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -54,6 +54,21 @@
 
 	{{-- LOCAL STYLES --}}
 	{{ HTML::style('styles/styles.css') }}
+	
+	{{-- EMBED CODE FOR FOCO FONT TEST - ADDED 5/20/2015 --}}
+	<script type="text/javascript">
+		(function() {
+			var path = '//easy.myfonts.net/v2/js?sid=221712(font-family=Foco+Bold)&sid=221718(font-family=Foco+Light)&sid=221747(font-family=Foco+Regular)&key=kZpFFm1YXB',
+				protocol = ('https:' == document.location.protocol ? 'https:' : 'http:'),
+				trial = document.createElement('script');
+			trial.type = 'text/javascript';
+			trial.async = true;
+			trial.src = protocol + path;
+			var head = document.getElementsByTagName("head")[0];
+			head.appendChild(trial);
+		})();
+	</script>
+	{{-- END: EMBED CODE FOR FOCO FONT TEST --}}
 
 </head>
 
