7f0e15583704a26bd8449c56b1ab97aed9e016e2
Kate deBethune <kdebethune@gmail.com>
2015-10-12 20:54:23 -0400

    removing Columbus day overlay and messaging

--- public/assets/js/jk-script.js
+++ public/assets/js/jk-script.js
@@ -6,7 +6,12 @@ $( document ).ready(function() {
 	$( "#menu-items > p:odd" ).css( "background-color", "#FFF9E8" );
 });
 */
-/*window.addEventListener("load", function load(event){ */
+
+/*
+ * Overlay for event-based messages, e.g. special closings
+ */
+
+/*
 window.addEventListener("load", function load(event) {
   var overlay = document.createElement('div');
   overlay.setAttribute('id', 'overlay');
@@ -18,17 +23,10 @@ window.addEventListener("load", function load(event) {
   msg.setAttribute('id', 'msg');
   msg.setAttribute('style', 'position: absolute;background-color: #FFF9E8; padding: 10px; width: 200px; height: 120px; font-size: .9em; z-index: 11; top: 50%; left: 50%; margin-left: -100px; margin-top: -100px;');
  
-  //msg.onclick = restore;
   document.body.appendChild(msg);
   
   msg.onclick = restore;
-  /*
-   msg.addEventListener('click', function () {
-    var a = document.getElementById('overlay');
-    var b = document.getElementById('msg');
-    document.body.removeChild(a);
-    document.body.removeChild(b);
-  }); */
+ 
   function restore() {
     var a = document.getElementById('overlay');
     var b = document.getElementById('msg');
@@ -37,3 +35,5 @@ window.addEventListener("load", function load(event) {
   }
   
 });
+*/
+
