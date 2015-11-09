4ed457e5e770fff8ca65dee7a5bf1077f18d41e2
Kate deBethune <kdebethune@gmail.com>
2015-11-09 15:36:40 -0500

    veteran's day overlay

--- public/assets/js/jk-script.js
+++ public/assets/js/jk-script.js
@@ -11,14 +11,14 @@ $( document ).ready(function() {
  * Overlay for event-based messages, e.g. special closings
  */
 
-/*
+
 window.addEventListener("load", function load(event) {
   var overlay = document.createElement('div');
   overlay.setAttribute('id', 'overlay');
   overlay.setAttribute('style', 'fontSize: 10px; background-color: #000; opacity: .5; filter: alpha(opacity=70); position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 10;');
   document.body.appendChild(overlay);
   var msg = document.createElement('div');
-  var txt = document.createTextNode('Judith\'s Kitchen will be closed for Columbus Day. Thank you! (Click to close)');
+  var txt = document.createTextNode('Judith\'s Kitchen will be closed for Veteran\'s Day. Thank you! (Click to close)');
   msg.appendChild(txt);
   msg.setAttribute('id', 'msg');
   msg.setAttribute('style', 'position: absolute;background-color: #FFF9E8; padding: 10px; width: 200px; height: 120px; font-size: .9em; z-index: 11; top: 50%; left: 50%; margin-left: -100px; margin-top: -100px;');
@@ -35,5 +35,5 @@ window.addEventListener("load", function load(event) {
   }
   
 });
-*/
+
 
