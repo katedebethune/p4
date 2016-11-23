43186896f7f75cdfec6e2e925117bad910ad82dd
Kate deBethune <kdebethune@gmail.com>
2016-11-23 08:52:35 -0500

    2016 Thanksgiving hours

--- public/assets/js/jk-script.js
+++ public/assets/js/jk-script.js
@@ -18,7 +18,7 @@ window.addEventListener("load", function load(event) {
   overlay.setAttribute('style', 'fontSize: 10px; background-color: #000; opacity: .5; filter: alpha(opacity=70); position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 10;');
   document.body.appendChild(overlay);
   var msg = document.createElement('div');
-  var txt = document.createTextNode('We will be closing at 6PM on Wednesday, November 23rd for the Thanksgiving holiday. We\'ll open again on Saturday, November 26th at 8AM.');
+  var txt = document.createTextNode('We will be closing at 6PM on Wednesday, November 23rd for the Thanksgiving holiday. We\'ll open again on Saturday, November 26th at 8AM. Best wishes to you and yours for the holiday!');
   msg.appendChild(txt);
   msg.setAttribute('id', 'msg');
   msg.setAttribute('style', 'position: absolute;background-color: #FFF9E8; padding: 10px; width: 280px; height: 160px; font-size: 1em; z-index: 11; top: 70%; left: 50%; margin-left: -130px; margin-top: -100px;');
